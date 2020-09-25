c8d3a3dc1466a83f04f09ad40b1f30de083f00d2
Syed Abidi <syed.abidi@catapultsports.com>
2020-09-25 22:01:18 +1000

    :tada: Initial Commit
    - Initial project setup
    - Laravel and React configuration completed
    - Docker configuration completed
    - Dev environment running through docker like a charm!

---
+++ resources/views/react-wrapper.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Product Listing</title>
+</head>
+<body>
+<div class="flex-center position-ref full-height">
+    <div id="root" class="content">
+    </div>
+</div>
+</body>
+<script type="text/javascript" src="../js/app.js"></script>
+</html>
